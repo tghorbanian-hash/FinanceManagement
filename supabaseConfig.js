7b87f6976f9ad1bf4cc278612b5e0aef80486f6b
tghorbanian-hash <t.ghorbanian@gmail.com>
2026-04-28 15:21:35 +0330

    Update supabaseConfig.js

--- supabaseConfig.js
+++ supabaseConfig.js
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // مقادیر مربوط به دیتابیس جدید خود را در اینجا قرار دهید
-const SUPABASE_URL = 'YOUR_NEW_SUPABASE_URL';
+const SUPABASE_URL = 'https://apmufgnvrncduflemjmk.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwbXVmZ252cm5jZHVmbGVtam1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNjgzNDUsImV4cCI6MjA5Mjk0NDM0NX0.ZtGRKgMTUK1SLm3665rBLNQOw7Zg_viEZVSZJxxim9M';
 
 window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
