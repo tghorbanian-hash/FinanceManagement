a5e78791826ba2d2c49031cad9b6326d7c948dfd
tghorbanian-hash <t.ghorbanian@gmail.com>
2026-05-01 17:50:22 +0330

    2

--- DesignSystem.js
+++ DesignSystem.js
@@ -1,56 +1,7 @@
 /* Filename: DesignSystem.js */
-import { 
-  Button, TextField, SelectField, ToggleField, CheckboxField, 
-  Card, Badge, PageHeader, Tabs, CurrencyField, TextAreaField, RadioGroup, 
-  Skeleton, EmptyState, StatCard, Timeline, Avatar, DropdownMenu, 
-  ProgressBar, AttachmentManager, DatePicker, Stepper, TagInput, Spinner 
-} from './DSCore.js';
-
-import { AdvancedFilter, DataGrid, LOVField } from './DSGrid.js';
-
-import { HighlightText, Tree, TreeGrid } from './DSTree.js';
-
-import { Modal, Tooltip, Alert, Toast, Banner, Dialog } from './DSFeedback.js';
-
-const DesignSystem = { 
-  Button, 
-  TextField, 
-  SelectField, 
-  ToggleField, 
-  CheckboxField, 
-  LOVField, 
-  Card, 
-  Badge, 
-  PageHeader, 
-  Modal, 
-  AdvancedFilter, 
-  AttachmentManager, 
-  Tabs, 
-  DataGrid, 
-  HighlightText, 
-  Tree, 
-  TreeGrid,
-  CurrencyField, 
-  TextAreaField, 
-  RadioGroup, 
-  Tooltip, 
-  Skeleton, 
-  EmptyState, 
-  StatCard, 
-  Timeline, 
-  Avatar, 
-  DropdownMenu, 
-  ProgressBar, 
-  DatePicker, 
-  Stepper, 
-  TagInput, 
-  Alert, 
-  Toast, 
-  Banner, 
-  Dialog, 
-  Spinner
+window.DesignSystem = {
+  ...(window.DSCore || {}),
+  ...(window.DSFeedback || {}),
+  ...(window.DSGrid || {}),
+  ...(window.DSTree || {})
 };
-
-window.DesignSystem = DesignSystem;
-
-export default DesignSystem;
